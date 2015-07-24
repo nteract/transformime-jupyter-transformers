c0ae162e4b160260aac00377625af2013e51afc1
Kyle Kelley <rgbkrk@gmail.com>
2015-07-24 10:14:50 -0700

    Bring in the common mark transformer.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {StreamTransformer} from './stream-transformer';
 import {TracebackTransformer} from './traceback-transformer';
+import {default as MarkdownTransformer} from 'transformime-commonmark';
 
 export default {StreamTransformer, TracebackTransformer};
