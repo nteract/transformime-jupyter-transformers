9e8ca7b1c4af304891ca2d30246d2083e8ce4dde
Kyle Kelley <rgbkrk@gmail.com>
2015-07-25 17:58:13 -0700

    Now cleaning up the package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {StreamTransformer} from './stream.transformer';
 import {TracebackTransformer} from './traceback.transformer';
+import {LaTeXTransformer} from './latex.transformer';
 import {MarkdownTransformer} from 'transformime-commonmark';
 
 export default {StreamTransformer, TracebackTransformer, MarkdownTransformer};
