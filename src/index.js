be0400352dd31e935cfc91066d8701240d6d6fea
Kyle Kelley <rgbkrk@gmail.com>
2015-07-26 14:53:54 -0700

    Bring on the PDFs!

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import {StreamTransformer} from './stream.transformer';
 import {TracebackTransformer} from './traceback.transformer';
 import {LaTeXTransformer} from './latex.transformer';
 import {MarkdownTransformer} from 'transformime-commonmark';
+import {PDFTransformer} from './pdf.transformer';
 
 export default {StreamTransformer, TracebackTransformer, MarkdownTransformer};
