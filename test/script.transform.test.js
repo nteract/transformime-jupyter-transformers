50eb66ce85ed42d6d486b2393ee0fb6cd4416d90
Kyle Kelley <rgbkrk@gmail.com>
2015-08-10 18:24:19 -0500

    Script tag verification.

--- test/script.transform.test.js
+++ test/script.transform.test.js
@@ -12,7 +12,24 @@ describe('script transform', function() {
             ]);
     });
 
-    it('should have the text/javascript mimetype', function() {
+    it('should create a script tag', function() {
         assert.equal(ScriptTransform.mimetype, 'text/javascript');
+
+        var promEl = this.t.transform(
+            {"text/javascript": "window.x = 1"},
+            this.document);
+
+        return promEl.then((bundle) => {
+            assert.equal(bundle.el.localName, "script");
+            assert.equal(bundle.el.textContent, "window.x = 1");
+
+            // Ensure script has not been run yet
+            assert.isUndefined(this.document.defaultView.x);
+            // Put it on the DOM
+            this.document.querySelector("body").appendChild(bundle.el);
+            // Should be evaluated now
+            assert.equal(this.document.defaultView.x, 1);
+
+        });
     });
 });
