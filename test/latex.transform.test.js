6021af528c4ceb3f0ffcb43921f4c763768dc0cc
Lukas Geiger <lukas.geiger94@gmail.com>
2016-04-25 01:03:45 +0200

    LaTeXTransform: test math delimiter escaping.

--- test/latex.transform.test.js
+++ test/latex.transform.test.js
@@ -20,8 +20,19 @@ describe('latex transform', function() {
     });
 
     it('should output the correct MathJax script', function() {
-        let latex = '\sum\limits_{i=0}^{\infty} \frac{1}{n^2}';
-        let mathJaxScript = '<script type="math/tex">\sum\limits_{i=0}^{\infty} \frac{1}{n^2}</script>';
+        let latex = '\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}';
+        let mathJaxScript = '<script type="math/tex">\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}</script>';
+        let transformed =  this.t.transform(
+            {'text/latex': latex},
+            this.document);
+        return transformed.then(({el}) => {
+            assert.equal(el.outerHTML, mathJaxScript);
+        });
+    });
+
+    it('should escape Latex math delimiters', function() {
+        let latex = '$x\\[ $$= \\]$ 2\\(5\\)$';
+        let mathJaxScript = '<script type="math/tex">x = $ 25</script>';
         let transformed =  this.t.transform(
             {'text/latex': latex},
             this.document);
